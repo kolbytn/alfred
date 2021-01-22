7e14d8bcd5572cc378d2b39bdcfd6ca717ae90c9
Kolby Nottingham <kolbytn@gmail.com>
2021-01-21 16:20:48 -0800

    Finished merge

--- models/model/seq2seq.py
+++ models/model/seq2seq.py
@@ -16,7 +16,6 @@ from importlib import import_module
 
 from env.thor_env import ThorEnv
 from models.nn.resnet import Resnet
-<<<<<<< HEAD
 from utils.video_record import VideoRecord
 
 
@@ -37,53 +36,6 @@ class ValidationProcesses:
         newprocess = Process(target=subprocess.call, args=(['python', EVALCUSTOM]))
         self.processes.append(newprocess)
         newprocess.start()
-=======
-
-# video recorder helper class
-from datetime import datetime
-import cv2
-class VideoRecord:
-    def __init__(self, path, name, fps=5):
-        """
-        param:
-            path: video save path (str)
-            name: video name (str)
-            fps: frames per second (int) (default=5)
-        example usage:
-            rec = VideoRecord('path/to/', 'filename', 10)
-        """
-        self.path = path
-        self.name = name
-        self.fps = fps
-        self.frames = []
-    def record_frame(self, env_frame):
-        """
-            records video frame in this object
-        param:
-            env_frame: a frame from thor environment (ThorEnv().last_event.frame)
-        example usage:
-            env = Thorenv()
-            lastframe = env.last_event.frame
-            rec.record_frame(lastframes)
-        """
-        curr_image = Image.fromarray(np.uint8(env_frame))
-        img = cv2.cvtColor(np.asarray(curr_image), cv2.COLOR_RGB2BGR)
-        self.frames.append(img)
-    def savemp4(self):
-        """
-            writes video to file at specified location, finalize video file
-        example usage:
-            rec.savemp4()
-        """
-        if len(self.frames) == 0:
-            raise Exception("Can't write video file with no frames recorded")
-        height, width, layers = self.frames[0].shape
-        size = (width,height)
-        out = cv2.VideoWriter(f"{self.path}{self.name}.mp4", 0x7634706d, self.fps, size)
-        for i in range(len(self.frames)):
-            out.write(self.frames[i])
-        out.release()
->>>>>>> 97c1031b0e26e9666afe6984b2744f02078dec92
 
 
 class Module(nn.Module):
